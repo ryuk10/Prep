35f680cc0662beeee2670b2f0d07e98d4e955b34
Oussama Atmani <borntolearn314@gmail.com>
2024-08-28 18:20:17 +0100

    prep

---
+++ cleanstr.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	result := ""
+	args := os.Args[1]
+	if len(os.Args) > 2 {
+		fmt.Println()
+	}
+
+	for i := 0; i < len(args); i++ {
+		if string(args[i]) != " " {
+			result += string(args[i])
+		}
+		if i != 0 && i < len(args)-1 && (string(args[i]) == " " && string(args[i+1]) != " ") {
+			result += " "
+		}
+
+	}
+	fmt.Println(result)
+}
